3973782c68bafe5dda71c09d2715befe651a7f92
James Derrick <jediblob@aol.com>
2021-07-23 18:57:31 +0100

    add foundational tables to setup script

---
+++ db/1_setup.sql
@@ -0,0 +1,31 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id serial PRIMARY KEY,
+    username varchar(50) NOT NULL,
+    password varchar(30) NOT NULL
+);
+
+DROP TABLE IF EXISTS user_details;
+
+CREATE TABLE user_details (
+    user_id int NOT NULL,
+    first_name varchar(200),
+    last_name varchar(200),
+    email_address varchar(256)
+);
+
+DROP TABLE IF EXISTS categories;
+
+CREATE TABLE categories (
+    id serial PRIMARY KEY,
+    category varchar(100) NOT NULL
+);
+
+DROP TABLE IF EXISTS habits;
+
+CREATE TABLE habits (
+    id serial PRIMARY KEY,
+    habit varchar(100) NOT NULL,
+    category_id int NOT NULL
+);

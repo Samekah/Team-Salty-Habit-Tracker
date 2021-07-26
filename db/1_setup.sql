362b6dd5cbef486fac845daca1679ea7e3a0ffdf
James Derrick <jediblob@aol.com>
2021-07-26 11:14:31 +0100

    add initial data

--- db/1_setup.sql
+++ db/1_setup.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
     id serial PRIMARY KEY,
-    username varchar(50) NOT NULL,
+    username varchar(50) UNIQUE,
     password varchar(30) NOT NULL
 );
 
@@ -12,20 +12,20 @@ CREATE TABLE user_details (
     user_id int NOT NULL,
     first_name varchar(200),
     last_name varchar(200),
-    email_address varchar(256)
+    email_address varchar(256) UNIQUE
 );
 
 DROP TABLE IF EXISTS categories;
 
 CREATE TABLE categories (
     id serial PRIMARY KEY,
-    category varchar(100) NOT NULL
+    category varchar(100) UNIQUE
 );
 
 DROP TABLE IF EXISTS habits;
 
 CREATE TABLE habits (
     id serial PRIMARY KEY,
-    habit varchar(100) NOT NULL,
+    habit varchar(100) UNIQUE,
     category_id int NOT NULL
 );

12dc6b07eabe3a3eaf94857d5626df956e1d1755
Rafika <RTuremis@hotmail.co.uk>
2021-07-29 21:22:24 +0100

    DOM tests working

--- client/__tests__/dashboard.test.js
+++ client/__tests__/dashboard.test.js
@@ -3,65 +3,115 @@ const path = require('path');
 const homeHtml = fs.readFileSync(path.resolve(__dirname, '../dashboard.html'), 'utf8');
 const dashboardHelpers = require('../assets/js/dashboard');
 
-
 describe('dashboard.html elements', () => {
-    beforeAll(() => {
-        document.documentElement.innerHTML = homeHtml.toString();
-    })
-    test('There is a head', () => {
-        let head = document.querySelector('head');
-     expect(head).toBeTruthy();
-    })
-    test('There is a body', () => {
-        let body = document.querySelector('body');
-        expect(body).toBeTruthy();
-    })
-    test('There is a header', () => {
-        let header = document.querySelector('header');
-        expect(header).toBeTruthy();
-    })
-    test('There is a Navbar', () => {
-        let navBar = document.querySelector('nav');
-        expect(navBar).toBeTruthy();
-    })
-    test('There is a main', () => {
-        let main = document.querySelector('main');
-        expect(main).toBeTruthy();
-    })
-    test("There is a Favicon", () => {
-        let favicon = document.querySelector("link[rel = 'icon']");
-        expect(favicon).toBeTruthy();
-    })
-    test("The favicon has an image",() => {
-        let favicon = document.querySelector("link[rel = 'icon']")
-        let faviconImage = favicon.getAttribute("href");
-        expect(faviconImage).not.toBe("#")
-    })
-})
+  beforeAll(() => {
+    document.documentElement.innerHTML = homeHtml.toString();
+  });
+  test('There is a head', () => {
+    let head = document.querySelector('head');
+    expect(head).toBeTruthy();
+  });
+  test('There is a body', () => {
+    let body = document.querySelector('body');
+    expect(body).toBeTruthy();
+  });
+  test('There is a header', () => {
+    let header = document.querySelector('header');
+    expect(header).toBeTruthy();
+  });
+  test('There is a Navbar', () => {
+    let navBar = document.querySelector('nav');
+    expect(navBar).toBeTruthy();
+  });
+  test('There is a section', () => {
+    let main = document.querySelector('section');
+    expect(main).toBeTruthy();
+  });
+  test('There is a Favicon', () => {
+    let favicon = document.querySelector("link[rel = 'icon']");
+    expect(favicon).toBeTruthy();
+  });
+  test('The favicon has an image', () => {
+    let favicon = document.querySelector("link[rel = 'icon']");
+    let faviconImage = favicon.getAttribute('href');
+    expect(faviconImage).not.toBe('#');
+  });
+});
 
 describe('dashboard helper fucntion', () => {
-    beforeAll(() => {
-        document.documentElement.innerHTML = homeHtml.toString();
-    })
+  beforeAll(() => {
+    document.documentElement.innerHTML = homeHtml.toString();
+  });
 
-    test("renderUsernameToDashboard displays the username in H1",() => {
-        dashboardHelpers.renderUsernameToDashboard("raf")
-        const newH1 = document.querySelector('#userInfo h1')
-        expect(newH1).toBeTruthy();
-        expect(newH1.textContent).toContain('raf');
-    })
+  test('renderUsernameToDashboard displays the username in H1', () => {
+    dashboardHelpers.renderUsernameToDashboard('raf');
+    const newH1 = document.querySelector('#userInfo h1');
+    expect(newH1).toBeTruthy();
+    expect(newH1.textContent).toContain('raf');
+  });
 
-    test("renderHabitToDashboard renders habit to DOM",() => {
-        let fakeData = [{
-            id:1,
-            habit:"drink water",
-            history:[]
-        }]
-        dashboardHelpers.renderHabitToDashboard(fakeData)
-        let updateButton = document.querySelector("#updateStreak-1")
-        expect(updateButton).toBeTruthy();
-        let streak = document.querySelector("#habitStreak-1")
-        expect(streak.textContent).toBe("0");
-    })
+  test('renderHabitToDashboard renders habit to DOM', () => {
+    let fakeData = [
+      {
+        id: 1,
+        habit: 'drink water',
+        history: [],
+      },
+    ];
+    dashboardHelpers.renderHabitToDashboard(fakeData);
+    let updateButton = document.querySelector('#updateStreak-1');
+    expect(updateButton).toBeTruthy();
+    let streak = document.querySelector('#habitStreak-1');
+    expect(streak.textContent).toBe('0');
+  });
 
-})
+  test('renderHabitToDashboard disables button if history has current date', () => {
+    let fakeData = [
+      {
+        id: 2,
+        habit: 'skincare',
+        history: ['2021-07-28', '2021-07-29'],
+      },
+    ];
+    dashboardHelpers.renderHabitToDashboard(fakeData);
+    let updateButton = document.querySelector('#updateStreak-2');
+    expect(updateButton.disabled).toBeTruthy();
+  });
+  test('renderHabitToDashboard enables button if history doesnt have current date', () => {
+    let fakeData = [
+      {
+        id: 3,
+        habit: 'run',
+        history: ['2021-07-27', '2021-07-28'],
+      },
+    ];
+    dashboardHelpers.renderHabitToDashboard(fakeData);
+    let updateButton = document.querySelector('#updateStreak-3');
+    expect(updateButton.disabled).toBeFalsy();
+  });
+  // change date tomorrow when testing for presentation
+  test('renderHabitToDashboard start streak if history has current date only', () => {
+    let fakeData = [
+      {
+        id: 4,
+        habit: 'jog',
+        history: ['2021-07-29'],
+      },
+    ];
+    dashboardHelpers.renderHabitToDashboard(fakeData);
+    let streak = document.querySelector('#habitStreak-4');
+    expect(streak.textContent).toBe('1');
+  });
+  test('renderHabitToDashboard start streak if history has current date only', () => {
+    let fakeData = [
+      {
+        id: 5,
+        habit: 'shower',
+        history: ['2021-07-27', '2021-07-28', '2021-07-29'],
+      },
+    ];
+    dashboardHelpers.renderHabitToDashboard(fakeData);
+    let streak = document.querySelector('#habitStreak-5');
+    expect(streak.textContent).toBe('3');
+  });
+});
--- client/assets/js/create_habit.js
+++ client/assets/js/create_habit.js
@@ -1,282 +1,259 @@
 //const baseApiUrl = 'https://habitual-2021.herokuapp.com';
 const baseApiUrl = 'http://localhost:3000';
 
-let habitHeading = document.querySelector("#instruction");
-let frequency = document.querySelector("#dropdown");
-let submit = document.querySelector("#submitHabit");
-let date = document.querySelector("#submitStartDate");
-let selectedDate = document.querySelector("#startDate");
+let habitHeading = document.querySelector('#instruction');
+let frequency = document.querySelector('#dropdown');
+let submit = document.querySelector('#submitHabit');
+let date = document.querySelector('#submitStartDate');
+let selectedDate = document.querySelector('#startDate');
 
-frequency.setAttribute("style", "display:none !important"); 
-submit.setAttribute("style", "display:none !important"); 
-date.setAttribute("style", "display:none !important");
+frequency.setAttribute('style', 'display:none !important');
+submit.setAttribute('style', 'display:none !important');
+date.setAttribute('style', 'display:none !important');
 
 let selectedHabitId;
 let habitFrequency;
-let startDate; 
+let startDate;
 
-window.onload = function(){
-    setup();
+window.onload = function () {
+  setup();
 };
 
-async function setup(){
-    habitHeading.style.marginLeft ="10px";
-    habitHeading.textContent = "category";
+async function setup() {
+  habitHeading.style.marginLeft = '10px';
+  habitHeading.textContent = 'category';
 
-    const categoryData = await getAllCategories();
-    let sectionToAppend = document.querySelector("#habitForm")
+  const categoryData = await getAllCategories();
+  let sectionToAppend = document.querySelector('#habitForm');
 
-    let buttonLength = categoryData.length;
-    
-    for(let i = 0; i< buttonLength; i++){
-        let categoryButton = document.createElement("button");
-        categoryButton.textContent = categoryData[i].category;
-        categoryButton.id = `category${i+1}`;
-        categoryButton.value = categoryData[i].id;
+  let buttonLength = categoryData.length;
 
-        let categoryDiv = document.createElement("div");
+  for (let i = 0; i < buttonLength; i++) {
+    let categoryButton = document.createElement('button');
+    categoryButton.textContent = categoryData[i].category;
+    categoryButton.id = `category${i + 1}`;
+    categoryButton.value = categoryData[i].id;
 
-        if(((buttonLength - 1)/3 === 1 && i === buttonLength-1) || buttonLength === 1){
-            categoryDiv.setAttribute("class", "col-12 d-flex justify-content-center")
-        }
-        else if (((buttonLength - 2)/3  === 1 && i === buttonLength-3) || buttonLength === 2){
-            categoryDiv.setAttribute("class", "col-6 d-flex justify-content-center") ;
-        }
-        else{
-            categoryDiv.setAttribute("class", "col d-flex justify-content-center");
-        }
+    let categoryDiv = document.createElement('div');
 
-        categoryDiv.append(categoryButton);
-        sectionToAppend.append(categoryDiv);
+    if (((buttonLength - 1) / 3 === 1 && i === buttonLength - 1) || buttonLength === 1) {
+      categoryDiv.setAttribute('class', 'col-12 d-flex justify-content-center');
+    } else if (((buttonLength - 2) / 3 === 1 && i === buttonLength - 3) || buttonLength === 2) {
+      categoryDiv.setAttribute('class', 'col-6 d-flex justify-content-center');
+    } else {
+      categoryDiv.setAttribute('class', 'col d-flex justify-content-center');
     }
 
-    let categoryItems = document.querySelectorAll("[id^='category']");
-    let allHabitData = await getAllHabits();
-    let specificHabitData = [];
-   
-    for(let i = 0; i < categoryItems.length; i++){
-        categoryItems[i].addEventListener("click", ()=>{
-            
-            selectedCategoryId = categoryItems[i].value;
-            
-            for(const habit of allHabitData){
-                if(habit.category_id == selectedCategoryId){
-                    specificHabitData.push(habit);
-                } 
-            }
+    categoryDiv.append(categoryButton);
+    sectionToAppend.append(categoryDiv);
+  }
 
-            habitHeading.textContent = "habit and frequency";
-            hideButtons(categoryItems);
-           
-            buttonLength = specificHabitData.length
+  let categoryItems = document.querySelectorAll("[id^='category']");
+  let allHabitData = await getAllHabits();
+  let specificHabitData = [];
 
-            for(let j = 0; j< buttonLength; j++){
-                let habitButton = document.createElement("button");
-                habitButton.textContent = specificHabitData[j].habit_name;
-                habitButton.value = 0;
-                habitButton.id = `habit${specificHabitData[j].id}`;
-                    
-                let habitDiv = document.createElement("div");
-        
-                     
-                if(((buttonLength - 1)/3 === 1 && j === buttonLength-1) || buttonLength === 1){
-                    habitDiv.setAttribute("class", "col-12 d-flex justify-content-center")
-                }
-                else if (((buttonLength - 2)/3  === 1 && j === buttonLength-3) || buttonLength === 2){
-                    habitDiv.setAttribute("class", "col-6 d-flex justify-content-center") ;
-                }
-                else{
-                    habitDiv.setAttribute("class", "col d-flex justify-content-center");
-                }
-        
-                habitDiv.append(habitButton);
-                sectionToAppend.append(habitDiv);
-            }
+  for (let i = 0; i < categoryItems.length; i++) {
+    categoryItems[i].addEventListener('click', () => {
+      selectedCategoryId = categoryItems[i].value;
 
-            frequency.setAttribute("style", "display:flex !important"); 
-            submit.setAttribute("style", "display:flex !important"); 
-            date.setAttribute("style", "display:flex !important");
-            habitFrequencySetup();
-        }); 
+      for (const habit of allHabitData) {
+        if (habit.category_id == selectedCategoryId) {
+          specificHabitData.push(habit);
+        }
+      }
 
-    }
+      habitHeading.textContent = 'habit and frequency';
+      hideButtons(categoryItems);
+
+      buttonLength = specificHabitData.length;
+
+      for (let j = 0; j < buttonLength; j++) {
+        let habitButton = document.createElement('button');
+        habitButton.textContent = specificHabitData[j].habit_name;
+        habitButton.value = 0;
+        habitButton.id = `habit${specificHabitData[j].id}`;
+
+        let habitDiv = document.createElement('div');
+
+        if (((buttonLength - 1) / 3 === 1 && j === buttonLength - 1) || buttonLength === 1) {
+          habitDiv.setAttribute('class', 'col-12 d-flex justify-content-center');
+        } else if (((buttonLength - 2) / 3 === 1 && j === buttonLength - 3) || buttonLength === 2) {
+          habitDiv.setAttribute('class', 'col-6 d-flex justify-content-center');
+        } else {
+          habitDiv.setAttribute('class', 'col d-flex justify-content-center');
+        }
+
+        habitDiv.append(habitButton);
+        sectionToAppend.append(habitDiv);
+      }
+
+      frequency.setAttribute('style', 'display:flex !important');
+      submit.setAttribute('style', 'display:flex !important');
+      date.setAttribute('style', 'display:flex !important');
+      habitFrequencySetup();
+    });
+  }
 }
 
-async function habitFrequencySetup(){
+async function habitFrequencySetup() {
+  let habitItems = document.querySelectorAll("button[id^='habit']");
 
-    let habitItems = document.querySelectorAll("button[id^='habit']");
+  for (let i = 0; i < habitItems.length; i++) {
+    habitItems[i].addEventListener('click', () => {
+      let btnState = habitItems[i].value;
+      let btncolor;
+      let btnHabitId = habitItems[i].id.split('habit')[1];
 
-    for(let i = 0; i < habitItems.length; i++){
-        habitItems[i].addEventListener("click", ()=>{
-            let btnState = habitItems[i].value;
-            let btncolor;
-            let btnHabitId = habitItems[i].id.split("habit")[1];
+      switch (btnState) {
+        case '0':
+          btncolor = 'green';
+          btnState = 1;
+          break;
+        case '1':
+          btncolor = '#FFE3BD';
+          btnState = 0;
+          break;
+      }
 
-            switch(btnState){
-                case "0":
-                    btncolor = "green";
-                    btnState = 1;
-                    break;
-                case "1":
-                    btncolor = "#FFE3BD";
-                    btnState = 0;
-                    break;
-            }
+      habitItems[i].value = btnState;
+      buttonStates(btnHabitId, btnState, habitItems);
+      habitItems[i].setAttribute('style', `background-color:${btncolor}`);
 
-            habitItems[i].value = btnState;
-            buttonStates(btnHabitId,btnState,habitItems);
-            habitItems[i].setAttribute("style", `background-color:${btncolor}`); 
+      selectedHabitId = btnHabitId;
+    });
+  }
 
-            selectedHabitId = btnHabitId;
-        })
-    }
-   
-    let todaysDate =  new Date().toISOString().split("T")[0]
-    selectedDate.min = todaysDate;
-    selectedDate.value = todaysDate;
+  let todaysDate = new Date().toISOString().split('T')[0];
+  selectedDate.min = todaysDate;
+  selectedDate.value = todaysDate;
 
-    const frequencyData = await getAllfrequencies();
-    let listToAppend = document.querySelector("#frequencyItems")
+  const frequencyData = await getAllfrequencies();
+  let listToAppend = document.querySelector('#frequencyItems');
 
-    for(k = 0; k < frequencyData.length; k++){
-        let listItem = document.createElement("li");
-        let listLink = document.createElement("a");
-        
-        listLink.setAttribute("class", "dropdown-item")
-        listLink.id = `${frequencyData[k].id}`;
+  for (k = 0; k < frequencyData.length; k++) {
+    let listItem = document.createElement('li');
+    let listLink = document.createElement('a');
 
-        listLink.textContent = frequencyData[k].frequency;
-        listItem.append(listLink);
+    listLink.setAttribute('class', 'dropdown-item');
+    listLink.id = `${frequencyData[k].id}`;
 
-        listToAppend.append(listItem);
+    listLink.textContent = frequencyData[k].frequency;
+    listItem.append(listLink);
 
-    }
+    listToAppend.append(listItem);
+  }
 
-    let dropdownItems = document.querySelectorAll(".dropdown-item");
+  let dropdownItems = document.querySelectorAll('.dropdown-item');
 
-    for(let j = 0; j < dropdownItems.length; j++){
+  for (let j = 0; j < dropdownItems.length; j++) {
+    dropdownItems[j].addEventListener('click', () => {
+      let interval = dropdownItems[j].textContent;
+      habitFrequency = dropdownItems[j].id;
 
-        dropdownItems[j].addEventListener("click", ()=>{
+      let dropdownWindow = document.querySelector('#dWindow');
 
-            let interval = dropdownItems[j].textContent
-            habitFrequency = dropdownItems[j].id;
+      dropdownWindow.textContent = interval;
+    });
+  }
 
-            let dropdownWindow = document.querySelector("#dWindow");
-
-            dropdownWindow.textContent = interval;
-
-        })
-    }
-    
-    let submitForm = document.querySelector("#submit");
-    submitForm.addEventListener("click", createHabit);
-
+  let submitForm = document.querySelector('#submit');
+  submitForm.addEventListener('click', createHabit);
 }
 
-async function createHabit(event){
-    event.preventDefault();
-    
-    if(!habitFrequency){
-        alert("Please select a frequency for your habit!")
+async function createHabit(event) {
+  event.preventDefault();
+
+  if (!habitFrequency) {
+    alert('Please select a frequency for your habit!');
+  } else {
+    let user = await fetchUserIDByUsername(localStorage.getItem('username'));
+
+    const data = {
+      habitId: selectedHabitId,
+      frequencyId: habitFrequency,
+      startDate: selectedDate.value,
+    };
+
+    try {
+      const options = {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: { 'Content-Type': 'application/json' },
+      };
+      const response = await fetch(`${baseApiUrl}/user/${user}/habits`, options);
+      const err = await response.json();
+
+      if (!err) {
+        throw Error(err);
+      } else {
+        window.location.assign('dashboard.html');
+      }
+    } catch (err) {
+      console.warn(err);
     }
-    else{
-        let user = await fetchUserIDByUsername(localStorage.getItem("username"))
-
-        const data ={
-            habitId: selectedHabitId,
-            frequencyId: habitFrequency,
-            startDate: selectedDate.value
-        }
-
-        try{
-            const options = {
-                method: "POST",
-                body: JSON.stringify(data),
-                headers:{ "Content-Type": "application/json" }
-            }
-
-            const response = await fetch(`${baseApiUrl}/user/${user}/habits`, options);
-            const err = await response.json();
-
-            if(!err){
-                throw Error(err);
-            }
-            else{
-                window.location.assign("dashboard.html");
-            }
-        }
-        catch(err){
-            console.warn(err);
-        }
-    }
+  }
 }
 
 async function fetchUserIDByUsername(uName) {
-    let response = await fetch(`${baseApiUrl}/user/${uName}`);
-    let userID = await response.json();
-    return userID;
+  let response = await fetch(`${baseApiUrl}/user/${uName}`);
+  let userID = await response.json();
+  return userID;
 }
 
-async function getAllCategories(){
-    try{
-        const response = await fetch(`${baseApiUrl}/category/`);
-        const data = await response.json();
-        return data;
-    } catch(err){
-        console.warn(err);
-    }
-
+async function getAllCategories() {
+  try {
+    const response = await fetch(`${baseApiUrl}/category/`);
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.warn(err);
+  }
 }
 
-async function getAllfrequencies(){
-    try{
-        const response = await fetch(`${baseApiUrl}/habit/frequency`);
-        const data = await response.json();
-        return data;
-    } catch(err){
-        console.warn(err);
-    }
+async function getAllfrequencies() {
+  try {
+    const response = await fetch(`${baseApiUrl}/habit/frequency`);
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.warn(err);
+  }
 }
 
-async function getAllHabits(id){
-    try{
-        const response = await fetch(`${baseApiUrl}/habit/`);
-        const data = await response.json();
-        return data;
-    } catch(err){
-        console.warn(err);
-    }
-
+async function getAllHabits(id) {
+  try {
+    const response = await fetch(`${baseApiUrl}/habit/`);
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.warn(err);
+  }
 }
 
-function hideButtons(object){
-    for(btn of object){
-        btn.style.margin = "0px";
-        btn.style.display = "none";
-    }
+function hideButtons(object) {
+  for (btn of object) {
+    btn.style.margin = '0px';
+    btn.style.display = 'none';
+  }
 }
 
-function buttonStates(id,state,object){
-    let clickable;
-    switch(state){
-        case 1:
-            clickable = true;
-            break;
-        case 0:
-            clickable = false;
-            break;
-    }
+function buttonStates(id, state, object) {
+  let clickable;
+  switch (state) {
+    case 1:
+      clickable = true;
+      break;
+    case 0:
+      clickable = false;
+      break;
+  }
 
-    for(btn of object){
-        
-        if(btn.id.endsWith(id)){
-          
-        }
-        else{
-            btn.disabled = clickable; 
-        }         
+  for (btn of object) {
+    if (btn.id.endsWith(id)) {
+    } else {
+      btn.disabled = clickable;
     }
-
+  }
 }
 
 const logOut = document.getElementById('logout');
